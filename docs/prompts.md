c6e0cac9b251038342593b87a3511acacac791ce
Brian C Sparks <briancsparks@gmail.com>
2023-03-20 17:25:50 -0700

    Refactoring

--- docs/prompts.md
+++ docs/prompts.md
@@ -1,8 +1,45 @@
 
+## Raylib
 
+```
 I want a C++ program (named xviz) that uses CMake and uses Raylib. Please include
 the typical Raylib draw loop, and inside this loop, draw a green pixel, a yellow
 line, a black bezier curve, a red circle, a brown ellipse, a lime ring, a teal
 rectangle, and a cyan triangle.
+```
+
+Note that 'TEAL' and 'CYAN' are not Raylib colors. (I changed them to SKYBLUE and
+VIOLET, respectively.)
+
+## Add in Kafka
+
+```
+Please enhance xviz by adding support for reading from Kafka. It should read all
+messages from the "svo" topic at the start of the raylib draw loop. The messages
+will contain the following JSON: '{"subject": "circle", "verb": "full", "objekt": "red"}'.
+Xviz should then draw a circle with the color that is in the "objekt" value, for
+every message read from the topic.
+```
+
+## Change to librdkafka
+
+GPT's response used cppkafka, which I couldn't get to work with CMake/vcpkg.
+
+```
+Can you please change to using librdkafka?
+```
+
+That mostly worked, but it got the Kafka header location wrong:
+
+```cpp
+#include "rdkafkacpp.h"       /* wrong */
+
+// -- instead of --
+
+#include "librdkafka/rdkafkacpp.h"
+```
+
+## whatever
+
 
 
--- main.cpp
+++ main.cpp
@@ -1,3 +1,95 @@
+#include "raylib.h"
+#include "librdkafka/rdkafkacpp.h"
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+void processMessage(const std::string& message, std::vector<Color>& circles) {
+  json j = json::parse(message);
+
+  if (j["subject"] == "circle" && j["verb"] == "full") {
+    std::string color_name = j["objekt"];
+    Color color;
+
+    if (color_name == "red") {
+      color = RED;
+    } else if (color_name == "green") {
+      color = GREEN;
+    } else if (color_name == "blue") {
+      color = BLUE;
+    } else {
+      color = RAYWHITE;
+    }
+
+    circles.push_back(color);
+  }
+}
+
+int main() {
+  const int screenWidth = 800;
+  const int screenHeight = 450;
+  const std::string kafka_topic = "svo";
+
+  std::string errstr;
+  RdKafka::Conf *conf = RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL);
+  conf->set("metadata.broker.list", "localhost:9092", errstr);
+  conf->set("enable.auto.commit", "true", errstr);
+
+  RdKafka::Consumer *consumer = RdKafka::Consumer::create(conf, errstr);
+  delete conf;
+
+  RdKafka::Conf  *topic_conf = RdKafka::Conf::create(RdKafka::Conf::ConfType::CONF_TOPIC);
+  RdKafka::Topic *topic = RdKafka::Topic::create(consumer, kafka_topic, topic_conf, errstr);
+  delete topic_conf;
+
+  consumer->start(topic, 0, RdKafka::Topic::OFFSET_BEGINNING);
+
+  std::vector<Color> circles;
+
+  InitWindow(screenWidth, screenHeight, "xviz");
+
+  while (!WindowShouldClose()) {
+    RdKafka::Message *msg = consumer->consume(topic, 0, 1000);
+
+    if (msg->err() == RdKafka::ERR_NO_ERROR) {
+      processMessage(static_cast<const char *>(msg->payload()), circles);
+    }
+
+    delete msg;
+
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    for (const auto &color: circles) {
+      DrawCircle(150, 150, 50, color);
+    }
+
+    EndDrawing();
+  }
+
+  consumer->stop(topic, 0);
+
+  CloseWindow();
+  delete topic;
+  delete consumer;
+  RdKafka::wait_destroyed(5000);
+
+  return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#if 0
 #include <iostream>
 #include "raylib.h"
 
@@ -27,3 +119,4 @@ int main() {
 
   return 0;
 }
+#endif
